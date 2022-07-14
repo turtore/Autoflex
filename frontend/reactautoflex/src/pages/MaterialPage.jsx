3d432b530114ba379725eb10be06a2246e025990
turtore <brunosap@gmail.com>
2022-07-14 19:29:06 -0300

    React: added UseState and form submit to register new product at ProductPage and MaterialPage

--- frontend/reactautoflex/src/pages/MaterialPage.jsx
+++ frontend/reactautoflex/src/pages/MaterialPage.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const MaterialPage = () => {
+    const [inputsState, setInputsState] = useState({
+      name: '',
+      quantity: '',
+    });
+    
+    const handleChange = ({ target: { name, value } }) => {
+      setInputsState({
+        ...inputsState,
+        [name]: value,
+      });
+    };
+
+    const handleClick = () => {
+      const material = {
+        name: inputsState.name,
+        quantity: inputsState.quantity,
+      };
+      console.log(material);
+    }
+        
+    
   return (
-    <div>MaterialPage</div>
+    <div>
+      <h2>Register a new Material</h2>
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          onChange={handleChange}
+          value={inputsState.name}
+          name="name"
+        />
+    
+        <label htmlFor="value">Quantity: </label>
+        <input
+          type="number"
+          onChange={handleChange}
+          value={inputsState.value}
+          name="quantity"
+        />
+
+        <button
+        onClick={handleClick}
+        >
+            register
+        </button>
+        
+      </div>
   )
 }
 
--- frontend/reactautoflex/src/pages/ProductPage.jsx
+++ frontend/reactautoflex/src/pages/ProductPage.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const ProductPage = () => {
+
+  const [inputsState, setInputsState] = useState({
+    name: '',
+    value: '',
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setInputsState({
+      ...inputsState,
+      [name]: value,
+    });
+  };
+
+  const handleClick = () => {
+    const product = {
+      name: inputsState.name,
+      value: inputsState.value,
+    };
+    console.log(product);
+  }
+    
+
   return (
     <div>
-        <h1> ProductPage </h1>
+      <h2>Register a new Product</h2>
+      <label htmlFor="name">Name: </label>
+      <input
+        type="text"
+        onChange={handleChange}
+        value={inputsState.name}
+        name="name"
+      />
+
+      <label htmlFor="value">Value: </label>
+      <input
+        type="number"
+        onChange={handleChange}
+        value={inputsState.value}
+        name="value"
+      />
+
+      <button
+        onClick={handleClick}
+      >
+          register
+      </button>
+      
     </div>
   )
 }
