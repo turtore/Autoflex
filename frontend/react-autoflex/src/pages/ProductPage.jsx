be28a575e0d82c124fe862da16bae6400cb07835
turtore <brunosap@gmail.com>
2022-07-18 22:11:13 -0300

    frontend: removed unused imports

--- frontend/react-autoflex/src/pages/ProductPage.jsx
+++ frontend/react-autoflex/src/pages/ProductPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Container, Button, Form, FloatingLabel } from 'react-bootstrap';
 import ProductCard from '../components/ProductCard';
 import Context from '../context/Context';
